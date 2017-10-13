dbc0ae354dc19ebef1aafd439cb9ae3d22352060
Matt David <mcdav3636@gmail.com>
2017-10-13 09:18:17 -0600

    added body content

--- index.php
+++ index.php
@@ -8,9 +8,27 @@
 
 		<h2>Persona</h2>
 
+		<p>	Cleatus is a 42 year old male who has visited the Albuquerque alrea during Baloon Fiesta for
+		10 years running. He is an avid promoter his favorite balloon gathering but has limited tallent and abilities on
+		his Gateway2000. </p>
+
 		<h2>User Story</h2>
 
+		<p> Cleatus is a registered user to the sight and would like to comment on the post.</p>
+
 		<h2>Use Case/ Interaction Flow</h2>
+
+		<P><storng>Story:</storng> As a registered user Cleatus would like to comment on the post.</P>
+
+		<ol>
+			<li>Cleatus clicks comment.</li>
+			<li>The sight loads the post, all previous comments, and a comment input field.</li>
+			<li>Cleatus clicks on the input field</li>
+			<li>The sight displays a coursor  in the text input field</li>
+			<li>Cleatus types his comment and presses submit button</li>
+			<li>The sight reloads to display original post, all previous comments including Cleatus's, and a
+			clear comment input field.</li>
+		</ol>
 
 		<h2>Conceptual Model</h2>
 	</body>
