2e88fb37cee38bfd04ca3ef6892bda774936a05d
Matt David <mcdav3636@gmail.com>
2017-10-18 00:48:05 -0600

    added sql statements

--- sql/reddit.sql
+++ sql/reddit.sql
@@ -23,3 +23,44 @@ CREATE TABLE comment (
 	commentDate DATETIME (6)
 	commentCommentId (16)
 )
+
+	INSERT INTO profile (profileId)
+	VALUES(
+	UNHEL(REPLACE("dc12ace9-3796-4902-931c-722e4f19bfd2"))
+
+	DELETE FROM profile WHERE profileHandle;
+	WHERE profileHandle Like%dolphins%;
+
+	UPDATE profilePhone
+	SET profilePhone = "+5058883323"
+	WHERE profileId = "dc12ace9-3796-4902-931c-722e4f19bfd2"
+
+	SELECT profileEmail
+	FROM profile
+	WHERE profileEmail Like "%@cnm%"
+
+
+	INSERT INTO post (postTitle)
+	VALUES("The great Bambino")
+
+	DELETE FROM post WHERE postContent LIKE "%mastermind%"
+
+	UPDATE postDate
+	Set postDate = "today"
+
+	SELECT postTitle
+	FROM post
+	WHERE profileHandle like "superheron"
+
+
+	INSERT INTO comment (commentContent)
+	VALUES("I wanna rock")
+
+	DELETE FROM comment WHERE commentDate < 06/01/2017
+
+	UPDATE commentTitle
+	SET commentTitle = "where code will take us"
+
+	SELECT commentId
+	FROM comment
+	WHEN commentContent LIKE "danceclub"
